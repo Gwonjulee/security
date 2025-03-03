8054122eba2dd6e54fdb28bbab66b40be40d4cc4
Gwonjuly <rnjswnfl99@gmail.com>
2025-03-03 20:52:04 +0900

    fix: 회원 가입 기타 수정

--- src/main/java/com/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/demo/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.demo.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +24,7 @@ public class WebSecurityConfig {
 //        http.formLogin(a -> a.defaultSuccessUrl("/main", true)); // 화면 로그인 허용
         http.csrf().disable();
         http.authorizeHttpRequests(a -> a.anyRequest().authenticated());
-        http.formLogin().disable();
+        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 
